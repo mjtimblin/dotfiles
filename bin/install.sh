f602bd99c6d0d1509e79c6bf2833fe2e355ebc51
Michael Timblin <mjtimblin@gmail.com>
2022-09-28 12:05:44 -0500

    Update install script to function as self-update script

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 
+# Update from remote repository
+git pull
+
 # Download submodules
 git submodule update --init --recursive
 
